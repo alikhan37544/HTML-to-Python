0d41ae8d70c999fbdb76235918a66c24f334eb00
Ali Khan <alikhan37544@gmail.com>
2023-06-15 13:09:48 +0530

    Trying to use built-in methods instead

--- Python-HTML-Calculator/app.py
+++ Python-HTML-Calculator/app.py
@@ -1,23 +1,48 @@
-from flask import Flask, request
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
 
-app = Flask(__name__)
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open('index.html', 'rb') as file:
+                self.wfile.write(file.read())
+        else:
+            self.send_response(404)
+            self.end_headers()
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+    def do_POST(self):
+        if self.path == '/calculate':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode('utf-8')
+            data = urllib.parse.parse_qs(post_data)
+            expression = data['expression'][0]
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    if request.method == 'POST':
-        expression = request.form['expression']
-        
-        try:
-            result = eval(expression)
-        except (SyntaxError, NameError):
-            result = "Invalid expression"
-        
-        return result
+            result = evaluate_expression(expression)
 
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(str(result).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def evaluate_expression(expression):
+    try:
+        result = eval(expression)
+    except (SyntaxError, NameError):
+        result = "Invalid expression"
+    
+    return result
+
+def run_server():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print('Server running on http://localhost:8000')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run()
+    run_server()
--- Python-HTML-Calculator/script.js
+++ Python-HTML-Calculator/script.js
@@ -1,29 +1,25 @@
-function appendToResult(value) {
-    document.getElementById("result").value += value;
-  }
-  
-  function calculateResult() {
-    var expression = document.getElementById("result").value;
+document.getElementById('calcForm').addEventListener('submit', calculateResult);
+
+function calculateResult(event) {
+    event.preventDefault();
     
-    // Create an HTTP request
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "calculate.py", true);
+    var expression = document.getElementById('expressionInput').value;
+    var resultContainer = document.getElementById('result');
     
-    // Set the content type header to send form data
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Define the callback function when the request completes
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        document.getElementById("result").value = xhr.responseText;
-      }
-    };
-    
-    // Send the expression to the Python file
-    xhr.send("expression=" + encodeURIComponent(expression));
-  }
-  
-  function clearResult() {
-    document.getElementById("result").value = "";
-  }
-  
+    fetch('/calculate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'expression=' + encodeURIComponent(expression)
+    })
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(result) {
+        resultContainer.textContent = 'Result: ' + result;
+    })
+    .catch(function(error) {
+        console.log('Error:', error);
+    });
+}
