4d4a64e3f5ac7cfdf373cfc8d91e3f22973ccbaf
Ali Khan <alikhan37544@gmail.com>
2023-06-16 16:24:03 +0530

    Fixed the Flask version
    Also it's connected to MySQL now

--- Python-HTML-Calculator(FLASK)/app.py
+++ Python-HTML-Calculator(FLASK)/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from collections.abc import MutableSet
+import pymysql
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -23,7 +25,21 @@ def calculate():
     elif operation == "divide":
         result = num1 / num2
 
+    connection = pymysql.connect(host='localhost',user='root',password='admin',db='calc_log')
+    cursor = connection.cursor()
+
+    sql="insert into result (outcome) values('%s');" %(result)
+    print(sql)
+    cursor.execute(sql)
+    connection.commit()
     return str(result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5500,debug=True)
+    connection = pymysql.connect(host='localhost',user='root',password='admin',db='calc_log')
+    cursor = connection.cursor()
+
+    sql="insert into result (outcome) values('%s');" %(result)
+    print(sql)
+    cursor.execute(sql)
+    connection.commit()
